d0b0f8173a3ce1ab77c696cfd4c1f68840e3cdb9
jrfm <moesch.jonas@protonmail.com>
2023-03-04 16:58:37 +0100

    add basic componentes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import "./App.css";
 import Header from "./Header";
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 20xx Jonas development. All rights reserved.</small>
-    </footer>
-  );
-}
-
-function MainContent() {
-  return (
-    <div>
-      <h1>Fun facts about React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-    </div>
-  );
-}
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 
 function App() {
   return (
